5c042ca41c79ebf09fb994f2762163097e57aadd
MaValentina <mariapinedabarroeta@gmail.com>
2022-06-25 20:51:13 -0400

    Stilos al Registro de Productos

--- view/register_item.php
+++ view/register_item.php
@@ -1,5 +1,9 @@
 <?php 
-require_once('../model/Producto.php');
+  require_once( '../model/User.php' ); 
+
+  $user = new User();
+
+  $request = $user->GetUsername();
 ?>
 
 <!DOCTYPE html>
@@ -8,23 +12,48 @@ require_once('../model/Producto.php');
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/register_item.css">
   <title>SistInventory ~ Registrar producto</title>
 </head>
 <body>
 
-  <header class="header">
+<header class="header">
 
+<h1 class="header__title"> Sist-Invenory </h1>
 
+<nav class="header__nav">
+  <ul>
+    <li class="nav__item"><a href='./inventory.php'> Inventario</a></li>
+    <li class="nav__item user">
+      <img src="../source/usuario.svg" alt="usuario">
+      <p class="username"><?php echo $request ?></p>
+    </li>
+  </ul>
+</nav>
 
-  </header>
+</header>
 
   <main class="container">
     <section class="container">
+
+    <h3>Registro de Productos</h3>
+      
     <form action="" class="form" name="form" method="POST">
-      <input type="text" name="name" class="name" placeholder="Nombre del Producto" minlength="6" maxlength="25" max="25" required>
+      <h2>•Informacion del Producto•</h2>
+      <label for="name">
+        Nombre 
+        <input type="text" name="name" class="name" placeholder="Nombre del Producto" minlength="6" maxlength="25" max="25" required>
+      </label>
+    <label for="price">
+      Precio
       <input type="number" name="price" class="price" placeholder="Precio del Producto" min="1" required>
+    </label>
+    <label for="cantidad">
+      Cantidad
       <input type="number" name="cantidad" class="cantidad" placeholder="Cantidad" min="1" required>
-      <input type="submit" name="submit" value="Agregar">
+    </label>
+
+      <input type="submit" name="submit" class="submit" value="Agregar">
     </form>
     </section>
 
